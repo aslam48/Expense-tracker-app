5723cefeb3679d4d8e88955d86a4744f906ed4ac
aslam48 <youngfizzy001@gmail.com>
2022-09-30 22:35:36 +0100

    expenses-tracker

--- src/Component/Transation.js
+++ src/Component/Transation.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Transation = ({transaction}) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
     
   return (
     <>
-        <li className='minus'>
-            {transaction.text} <span>-$400</span> <button className='delate-btn'>X</button>
+        <li className={transaction.amount < 0 ? "minus" : "plus"}>
+            {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span> <button className='delate-btn'>X</button>
         </li>
     </>
   )
